937d281b861901f83785d68821b27d4819ebf633
waytoni <168102732+upulgun@users.noreply.github.com>
2025-03-22 23:22:31 +1100

    Updated Sections 6 and 12

--- scripts/docx2html_full.py
+++ scripts/docx2html_full.py
@@ -11,31 +11,31 @@ output_folder = 'content'
 
 pages_info = [
     ("Introduction.txt", "Introduction.docx", "II."),
-  #  ("Acknowledgements.txt", "Acknowledgements.docx", "IV."),
-  #  ("page1.txt", "Section 1 - Birth.docx", "1."),
-  #  ("page2.txt", "Section 2 - Childhood and Family of the Buddha.docx", "2."),
-  #  ("page3.txt", "Section 3 - Asetic life.docx", "3."),
-  #  ("page4.txt", "Section 4 - Enlightenment.docx", "4."),
-  #  ("page4_1.txt", "Section 4-1 Four Assembly.docx", "4-1."),
+   # ("Acknowledgements.txt", "Acknowledgements.docx", "IV."),
+    ("page1.txt", "Section 1 - Birth.docx", "1."),
+   # ("page2.txt", "Section 2 - Childhood and Family of the Buddha.docx", "2."),
+   # ("page3.txt", "Section 3 - Asetic life.docx", "3."),
+   # ("page4.txt", "Section 4 - Enlightenment.docx", "4."),
+   # ("page4_1.txt", "Section 4-1 Four Assembly.docx", "4-1."),
     ("page5.txt", "Section 5 - Greatness of The Buddha.docx", "5."),
-    ("page6.txt", "Section 6 - Titles.docx", "6.")
+  #  ("page6.txt", "Section 6 - Titles.docx", "6.")
   #  ("page7.txt", "Section 7 - As a Teacher.docx", "7."),
-  #  ("page8.txt", "Section 8 - Praise and blame.docx", "8."),
-  #  ("page9.txt", "Section 9 - Past lives.docx", "9."),
-  #  ("page10.txt", "Section 10 - Dhamma.docx", "10."),
-  #  ("page11_1.txt","Section 11-1 Advice to the Sangha.docx", "11.1"),
-  #  ("page11_2.txt","Section 11-2 Advice to Lay Followers.docx", "11.2"),
-  #  ("page12.txt","Section 12 - Similies.docx", "12.")
+    ("page8.txt", "Section 8 - Praise and blame.docx", "8."),
+    ("page9.txt", "Section 9 - Past lives.docx", "9."),
+    ("page10.txt", "Section 10 - Dhamma.docx", "10."),
+    ("page11_1.txt","Section 11-1 Advice to the Sangha.docx", "11.1"),
+    ("page11_2.txt","Section 11-2 Advice to Lay Followers.docx", "11.2"),
+    ("page12.txt","Section 12 - Similies.docx", "12.")
   #  ("TOC.txt","Table of Contents 1.docx", "I."),
   #  ("Gratitude.txt","Gratitude to the Blessed One.docx", "III."),
   #  ("Abbreviations.txt","Abbreviations.docx", "V.")
     
 ]
 
-html_tags = ["<html", "<head", "<body", "<div", "<span", "<script", "<style", "<p", "<a", "<ul", "<ol", "<li",
+html_tags = ["<html", "<head", "<body", "<div", "<span", "<script", "<style", "<p", "<ul", "<ol", "<li",
             "<table", "<tr", "<td", "<th", "<form", "<input", "<button", "<select", "<option", "<meta", "<link",
             "<pre", "<em", "<p", "<h1", "<h2", "<h3", "<h4", "<h5", "<h6", "<img", "<br", "<hr", "<!--", "<title",
-            "</html", "</head", "</body", "</div", "</span", "</script", "</style", "</p", "</a", "</ul", "</ol",
+            "</html", "</head", "</body", "</div", "</span", "</script", "</style", "</p",  "</ul", "</ol",
             "</li", "</table", "</tr", "</td", "</th", "</form", "</input", "</button", "</select", "</option", 
             "</h1", "</h2", "</h3", "</h4", "</h5", "</h6", "</em", "</pre", "</p>"] 
 
@@ -60,7 +60,7 @@ if allFiles == True:
        
         print(docfile)
         page_content = docx2python(docfile, html = True)
-        print(page_content.text)
+        #print(page_content.text)
         soup = BeautifulSoup(page_content.text, "html.parser")
         for span in soup.find_all("span", attrs={"style": True}):
             span.unwrap()  # Removes the tag but keeps its content
@@ -76,8 +76,18 @@ if allFiles == True:
         with open(outputfile, "w", encoding="utf-8") as html_file:
             html_file.write(f'<div class="container">\n')
             html_file.write(f'<article class="pagewidth">\n')
+            capture_next = False
+            capture_next_now = False
             for line in lines:
                 stripped_line = line.strip()
+                #print(stripped_line)
+                if re.match(r'----media/.*\.(jpeg|jpg|gif|png)----', stripped_line):
+                    capture_next = True
+                    capture_next_now = True
+                    print('Image line detected ............')
+                    print(stripped_line)
+                    #outfile.write(line)  # Write the media line as is
+                    #continue
                 
                 stripped_line = re.sub(r'----endnote(\d+)----', r'<span id="Endnote\1">[<a href="#endnote\1">\1</a>]</span>', stripped_line)
                 stripped_line = re.sub(r'endnote(\d+)\)', r'<span id="endnote\1">[<a href="#Endnote\1">\1</a>]</span>', stripped_line)
@@ -87,10 +97,28 @@ if allFiles == True:
                 stripped_line = re.sub(r'----Image alt text----&gt;.*?&lt;', '', stripped_line)
                 
                 
+                if capture_next_now:
+                    print(stripped_line)
+                
                 if not any(stripped_line.startswith(tag) for tag in html_tags): 
                     if firstLine and stripped_line:
                         html_file.write(f'<h1>{stripped_line}</h1>\n')
                         firstLine = False
+                    
+                    
+                    
+                    elif capture_next and stripped_line:
+                        # Check if the line does not contain HTML header tags
+                        print(stripped_line)
+                        if not re.search(r'<(h1|h2|h3|h4|h5|h6)[^>]*>', stripped_line, re.IGNORECASE):
+                            # Enclose the line in <p class="caption"> </p>
+                            html_file.write(f'<p class="caption">{stripped_line}</p>\n')
+                        else:
+                            # If it contains header tags, write it as is
+                            html_file.write(line)
+                        capture_next = False
+                   
+                    
                     elif stripped_line.startswith('▲'):
                         html_file.write(f'<h2>{stripped_line}</h2>\n')
                     #elif stripped_line.startswith('♦'):
@@ -98,7 +126,14 @@ if allFiles == True:
                     else:                
                         html_file.write(f'<p>{stripped_line}</p>\n')
                 else:
+                    
                     html_file.write(f'{stripped_line}\n')  
+                    if capture_next_now:
+                        print(f'****** {stripped_line}')
+                        capture_next_now = False
+                    else:    
+                        capture_next = False
+                        
             html_file.write(f'</article>\n')
             html_file.write(f'</div>\n')
 
